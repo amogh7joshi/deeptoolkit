be25ac5a716d833f348d65588f54ac863a245988
amogh7joshi <joshi.amoghn@gmail.com>
2021-02-09 22:32:12 -0500

    Add new Hybrid Pooling layer

--- deeptoolkit/blocks/hybrid.py
+++ deeptoolkit/blocks/hybrid.py
@@ -6,11 +6,12 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Layer
 from tensorflow.keras.layers import Conv2D, BatchNormalization, DepthwiseConv2D, SeparableConv2D
+from tensorflow.keras.layers import AveragePooling2D, MaxPooling2D
 from tensorflow.keras.layers import Activation, InputLayer, Concatenate
 
 __all__ = ['HybridConvolution']
 
-class HybridConvolution(object):
+class HybridConvolution(Layer):
    """Constructs a hybrid convolution block for use in a convolutional neural network.
 
    This layer constructs two individual Convolution and Separable Convolution layers, and then concatenates
@@ -116,4 +117,64 @@ class HybridConvolution(object):
 
       # Merge branches and return.
       x = Concatenate()([conv_x, sep_conv_x])
+      return x
+
+class HybridPooling(Layer):
+   """Constructs a hybrid convolution block for use in a convolutional neural network.
+
+   This layer constructs two individual Convolution and Separable Convolution layers, and then concatenates
+   them together, forming a hybrid convolution layer, for use in deeper model architectures.
+
+   Usage:
+
+   The layer can be used in a Sequential model as would be any other layer.
+
+   >>> model = Sequential()
+   >>> model.add(HybridConvolution(32, kernel_size = (3, 3), activation = 'relu'))
+
+   Or it can be used in a Functional model, as would be any other layer.
+
+   >>> input = Input((256, 256, 3))
+   >>> output = HybridPooling(pool_size = (2, 2))(input)
+   >>> model = Model(input, output)
+
+   Parameters:
+      - All of the parameters in the network are the same as those of the Conv2D/SeparableConv2D & BatchNorm
+        layers with a few left out because they are unnecessary in this circumstance.
+      - Activation can either be directly following the convolution layer or after batch normalization,
+        which can be set by use of the `activation_point` parameter: True means before, False means after.
+   Returns:
+      - A layer instance consisting of convolution, batch normalization, and activation.
+   """
+   def __init__(self, pool_size = (2, 2), strides = (1, 1), padding = 'valid'):
+      super(HybridPooling, self).__init__()
+
+      # Set class parameters.
+      if isinstance(pool_size, int):
+         self.pool_size = (pool_size, pool_size)
+      else:
+         self.pool_size = pool_size
+      if isinstance(strides, int):
+         self.strides = (strides, strides)
+      else:
+         self.strides = strides
+      self.padding = padding
+
+      # Set class layers.
+      try:
+         self.max_pool = MaxPooling2D(pool_size = pool_size, strides = strides, padding = padding)
+         self.avg_pool = AveragePooling2D(pool_size = pool_size, strides = strides, padding = padding)
+      except Exception as e:
+         raise e
+
+   def call(self, inputs, **kwargs):
+      # When layer is called from model.
+      # Max pooling branch.
+      max_pool_x = self.max_pool(inputs)
+
+      # Average pooling branch.
+      avg_pool_x = self.avg_pool(inputs)
+
+      # Merge branches and return.
+      x = Concatenate()([max_pool_x, avg_pool_x])
       return x
