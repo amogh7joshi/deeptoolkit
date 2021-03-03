8aeb45c6bca6fd857eb4303e01e2f561ad5fe68a
amogh7joshi <joshi.amoghn@gmail.com>
2021-03-03 14:36:11 -0500

    Added method to convert Keras architecture to PyTorch architecture

--- deeptoolkit/internal/beautification.py
+++ deeptoolkit/internal/beautification.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding = utf-8 -*-
 import re
+from collections import defaultdict
+
+from deeptoolkit.internal.helpers import DefaultDictionary
 
 LOG_DICT_NAMES = {
    'acc': 'Training Accuracy',
@@ -10,4 +13,25 @@ LOG_DICT_NAMES = {
    'loss': 'Training Loss',
    'val_loss': 'Validation Loss'
 }
+
+ACTIVATION_FUNCTION_NAMES = {
+   'relu': 'ReLU',
+   'leakyrelu': 'LeakyReLU',
+   'sigmoid': 'Sigmoid',
+   'softmax': 'Softmax'
+}
+
+VALID_CONVERSION_LAYER_NAMES = (
+   'conv2d', 'batchnormalization', 'upsampling2d',
+   'linear', 'dropout', 'input', 'depthwiseconv2d',
+   'separable_conv2d', 'maxpooling2d', 'averagepooling2d',
+   'flatten', 'activation', 'dense', 'relu', 'softmax'
+)
+
+LAYER_TORCH_CONVERSIONS = DefaultDictionary()
+LAYER_TORCH_CONVERSIONS['Separable_conv2d'] = 'SEPARABLE_CONV'
+LAYER_TORCH_CONVERSIONS['Depthwise_conv2d'] = 'DEPTHWISE_CONV'
+LAYER_TORCH_CONVERSIONS['Dense'] = 'Linear'
+LAYER_TORCH_CONVERSIONS['Batchnormalization'] = 'BatchNorm2d'
+
 
--- deeptoolkit/internal/helpers.py
+++ deeptoolkit/internal/helpers.py
@@ -3,6 +3,11 @@
 import tensorflow as tf
 from tensorflow.keras import backend as K
 
+class DefaultDictionary(dict):
+   """A special dictionary to return the default key if it is missing."""
+   def __missing__(self, key):
+      return key
+
 def select_threshold(tensor, thresh = None):
    """Returns a tensor with items greater than the provided threshold set to 1, else 0."""
    if thresh is None: # If threshold is not provided.
