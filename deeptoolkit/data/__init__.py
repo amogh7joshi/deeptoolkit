2c2643384209bb132bf24dc89903fbc97c01682f
amogh7joshi <joshi.amoghn@gmail.com>
2021-03-18 14:53:09 -0400

    Moved new image data gathering method to data module __init__

--- deeptoolkit/data/__init__.py
+++ deeptoolkit/data/__init__.py
@@ -1,2 +1,3 @@
 from .dataset_ops import reduce_dataset, shuffle_dataset, train_val_test_split
 from .data_visualization import plot_data_cluster, plot_class_distribution
+from .gather_data import gather_images
